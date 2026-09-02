350c984710de5a44da7830d12dc3f736f3851d55
Kerry Luo <kerryluo1@gmail.com>
2026-09-02 14:28:55 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,44 @@
+# binary search rows until you get to a row[0] < target 
+# and the next row[0] > target
+
+# then regular binary search
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        bottom, top = 0, len(matrix) - 1
+
+        while bottom <= top:
+            mid = (top + bottom) // 2
+
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] > target:
+                top = mid - 1
+            else:
+                bottom = mid + 1
+
+        # the correct row is always top? 
+
+        row = matrix[top]
+
+        bottom, top = 0, len(row) - 1
+
+        while bottom <= top:
+            mid = (top + bottom) // 2
+
+            if row[mid] == target:
+                return True
+            elif row[mid] > target:
+                top = mid - 1
+            else:
+                bottom = mid + 1
+
+        return False
+
+
+
+
+         
+
+
+        
